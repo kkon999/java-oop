816e6fd7f965d4710ace6b7d50574bd7b42243c1
kkon999 <gon910910@gmail.com>
2020-03-11 00:00:47 +0900

    java_enum02

--- java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo2.java
+++ java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo2.java
@@ -1,5 +1,22 @@
 package org.opentutorials.javatutorials.constant2;
+		public class ConstantDemo2 {
+	    private final static int APPLE = 1;
+	    private final static int PEACH = 2;
+	    private final static int BANANA = 3;
+	    public static void main(String[] args) {
+	        int type = APPLE;
+	        switch(type){
+	            case APPLE:
+	                System.out.println(40+" kcal");
+	                break;
+	            case PEACH:
+	                System.out.println(20+" kcal");
+	                break;
+	            case BANANA:
+	                System.out.println(93+" kcal");
+	                break;
+	        }
+	    }
+	}
 
-public class ConstantDemo2 {
 
-}
