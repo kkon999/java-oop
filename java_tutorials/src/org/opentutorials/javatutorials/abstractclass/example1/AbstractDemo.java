49443bce87537919cfa861cb97a09c9a544da2c4
kkon999 <gon910910@gmail.com>
2020-03-11 23:40:17 +0900

    2020_03_11

--- java_tutorials/src/org/opentutorials/javatutorials/abstractclass/example1/AbstractDemo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/abstractclass/example1/AbstractDemo.java
@@ -7,8 +7,7 @@ abstract class A{
     //본체가 있는 메소드는 abstract 키워드를 가질 수 없다.
     //public abstract int c(){System.out.println("Hello")}
     //추상 클래스 내에는 추상 메소드가 아닌 메소드가 존재 할 수 있다. 
-    public void d(){
-    	
+    public void d(){	
         System.out.println("Hello world");
     }
 }
@@ -16,7 +15,7 @@ public class AbstractDemo {
 	
     public static void main(String[] args) {
     	
-        A obj = new A();
+        //A obj = new A();
     }
 }
 
