ffe0513007f4cbf431e3e8919e2a2d0aa4a1af90
kkon999 <gon910910@gmail.com>
2020-03-14 23:23:48 +0900

    java-enum04

---
+++ java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo4.java
@@ -0,0 +1,31 @@
+package org.opentutorials.javatutorials.constant2;
+
+enum Fruit{
+    APPLE, PEACH, BANANA;
+}
+enum Company{
+    GOOGLE, APPLE, ORACLE;
+}
+ 
+public class ConstantDemo4 {
+     
+    public static void main(String[] args) {
+        /*
+        if(Fruit.APPLE == Company.APPLE){
+            System.out.println("과일 애플과 회사 애플이 같다.");
+        }
+        */
+        Fruit type = Fruit.APPLE;
+        switch(type){
+            case APPLE:
+                System.out.println(57+" kcal");
+                break;
+            case PEACH:
+                System.out.println(34+" kcal");
+                break;
+            case BANANA:
+                System.out.println(93+" kcal");
+                break;
+        }
+    }
+}
