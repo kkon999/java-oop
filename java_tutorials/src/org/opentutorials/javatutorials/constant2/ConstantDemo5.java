6643b0ff5ad7cf9c93021afaf4460a771abcf525
kkon999 <gon910910@gmail.com>
2020-03-14 23:32:55 +0900

    java-enum05

---
+++ java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo5.java
@@ -0,0 +1,35 @@
+package org.opentutorials.javatutorials.constant2;
+
+enum Fruit5{
+    APPLE("red"), PEACH("pink"), BANANA("yellow");
+    private String color;
+    Fruit5(String color){
+        System.out.println("Call Constructor "+this);
+        this.color = color;
+    }
+    String getColor(){
+        return this.color;
+    }
+}
+ 
+enum Company5{
+    GOOGLE, APPLE, ORACLE;
+}
+ 
+public class ConstantDemo5 {
+     
+    public static void main(String[] args) {
+        Fruit5 type = Fruit5.APPLE;
+        switch(type){
+            case APPLE:
+                System.out.println(57+" kcal, "+Fruit5.APPLE.getColor());
+                break;
+            case PEACH:
+                System.out.println(34+" kcal"+Fruit5.PEACH.getColor());
+                break;
+            case BANANA:
+                System.out.println(93+" kcal"+Fruit5.BANANA.getColor());
+                break;
+        }
+    }
+}
