79e2b6e436de5cad37ae29b12d5ad6eedbd33781
kkon999 <gon910910@gmail.com>
2020-03-21 01:21:57 +0900

    java-enum03-4,enum04

--- java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo3_4.java
+++ java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo3_4.java
@@ -40,6 +40,4 @@ public static void main(String[] args) {
 		break;
 	}
 }
-	
-
 }
--- java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo4.java
+++ java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo4.java
@@ -1,31 +1,52 @@
 package org.opentutorials.javatutorials.constant2;
 
-enum Fruit{
-    APPLE, PEACH, BANANA;
+
+// enum은 서로 연관된 상수들의 집합이다. 즉 바뀌지 않는 값들의 집합
+
+/* 아래에 있는 enum Fruit로 변경
+ * class Fruit{
+public static final Fruit APPLE = new Fruit();
+public static final Fruit PEACH = new Fruit();
+public static final Fruit BANANA = new Fruit();
+}*/
+
+/*class Company{
+public static final Company GOOGLE = new Company();
+public static final Company APPLE= new Company();
+public static final Company ORACLE= new Company();
 }
-enum Company{
-    GOOGLE, APPLE, ORACLE;
+*/
+
+enum Fruit4{
+	APPLE , PEACH , BANANA
 }
- 
+
+enum Company4{
+	GOOGLE , APPLE , ORACLE
+}
+
+
 public class ConstantDemo4 {
-     
-    public static void main(String[] args) {
-        /*
-        if(Fruit.APPLE == Company.APPLE){
-            System.out.println("과일 애플과 회사 애플이 같다.");
-        }
-        */
-        Fruit type = Fruit.APPLE;
-        switch(type){
-            case APPLE:
-                System.out.println(30+" kcal");
-                break;
-            case PEACH:
-                System.out.println(34+" kcal");
-                break;
-            case BANANA:
-                System.out.println(93+" kcal");
-                break;
-        }
-    }
+
+	public static void main(String[] args) {
+
+
+		Fruit4 type = Fruit4.APPLE;
+
+//switch 문을 사용 할 때 한가지 주의 할 것은 제한된 데이터 타입만을 사용할 수 있다. 
+//byte,short,char,int,enum,String,Character.Byte,Short,integer를 사용 가능하다.
+// 즉 if 문을 사용하거나 Fruit말고 상기에 있는 데이터 타입중 하나로 바꿔야 한다.
+
+			switch (type) {
+				case APPLE:
+					System.out.println(35 + "kcal");
+					break;
+				case PEACH:
+					System.out.println(57 + "kcal");
+					break;
+				case BANANA:
+					System.out.println(100 + "kcal");
+					break;
+		}
+	}
 }
