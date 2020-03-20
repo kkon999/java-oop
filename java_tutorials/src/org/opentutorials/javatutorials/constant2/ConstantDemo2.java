991902ac30aab7f12916f490caa983cf141c0ed0
kkon999 <gon910910@gmail.com>
2020-03-20 23:29:56 +0900

    java-enum02

--- java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo2.java
+++ java_tutorials/src/org/opentutorials/javatutorials/constant2/ConstantDemo2.java
@@ -1,22 +1,27 @@
 package org.opentutorials.javatutorials.constant2;
+
 		public class ConstantDemo2 {
-	    private final static int APPLE = 1;
-	    private final static int PEACH = 2;
-	    private final static int BANANA = 3;
-	    public static void main(String[] args) {
-	        int type = APPLE;
-	        switch(type){
-	            case APPLE:
-	                System.out.println(10+" kcal");
-	                break;
-	            case PEACH:
-	                System.out.println(20+" kcal");
-	                break;
-	            case BANANA:
-	                System.out.println(93+" kcal");
-	                break;
-	        }
-	    }
+			
+		// ConstaantDemo 와 다르게 APPLE,PEACH,BANANA의 변수를 상수화 하여 생성하여 구분지음 주석이 필요없게 된다.
+			
+		private final static int APPLE  =1;
+		private final static int PEACH  =2;	
+		private final static int BANANA =3;
+		
+		public static void main(String[] args) {
+			int type = APPLE;
+			
+			switch(type) {
+			case APPLE:
+				System.out.println(30 + " kacl");
+				break;
+			case PEACH:
+				System.out.println(57 + " kacl");
+				break;
+			case BANANA:
+				System.out.println(100 + " kacl");
+				break;
+			}
+		}
 	}
-
-
+		
