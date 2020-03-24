b98f3db9cfba4833cac48c1c6137cd2a4c36d641
kkon999 <gon910910@gmail.com>
2020-03-24 11:23:00 +0900

    java-Reference01,02

--- java_tutorials/src/org/opentutorials/javatutorials/reference/ReferenceDemo1.java
+++ java_tutorials/src/org/opentutorials/javatutorials/reference/ReferenceDemo1.java
@@ -2,16 +2,17 @@ package org.opentutorials.javatutorials.reference;
 
 
 public class ReferenceDemo1 {
- 
-    public static void runValue(){
-        int a = 1;
-        int b = a;
-        b = 2;
-        System.out.println("runValue, "+a); 
-    }
- 
-    public static void main(String[] args) {
-        runValue();
-    }
- 
+	
+	public static void runValue() {
+		
+		int a = 1;
+		int b = a; 	//b에 a의 값을 대입함
+		b = 2;		//b 값을 2로 변경
+		System.out.println("runValue : " + a); // 변수 b는 a의 값을 복제 한 것이기 때문에 b의 값이 변경 되더라도 a의 값은 변경 되지 않는다.
+	}
+	
+	public static void main(String[] args) {
+		runValue();
+		
+	}
 }
--- java_tutorials/src/org/opentutorials/javatutorials/reference/ReferenceDemo1_2.java
+++ java_tutorials/src/org/opentutorials/javatutorials/reference/ReferenceDemo1_2.java
@@ -1,30 +1,32 @@
 package org.opentutorials.javatutorials.reference;
 
+
+	//A 클래스를 생성하여 새로운 id 인스턴스를 생성
 class A{
-    public int id;
-    A(int id){
-        this.id = id;
-    }
+	public int id;
+	A(int id){
+		this.id = id;
+	}
 }
+
 public class ReferenceDemo1_2 {
- 
-    public static void runValue(){
-        int a = 1;
-        int b = a;
-        b = 2;
-        System.out.println("runValue, "+a); 
-    }
-     
-    public static void runReference(){
-        A a = new A(1);
-        A b = a;
-        b.id = 2;
-        System.out.println("runReference, "+a.id);      
-    }
- 
-    public static void main(String[] args) {
-        runValue();
-        runReference();
-    }
- 
+	public static void runValue() {
+		int a = 1;
+		int b = a;
+		b = 2;
+		System.out.println("runValue : " + a);
+	}
+	
+	
+	public static void runReference() {
+		A a = new A(1); // runValue와는 다르게 클래스 A를 생성하여 새로운 인스턴스 A(1)의 값은 메모리 공간에 저장 된다.그리고 변수 a는 인스턴스 A(1) 공간의 위치에 대한 주소를 저장
+		A b = a;		// 변수 b도 마찬가지로 변수a가 가리키고 있는 인스턴스 공간의 주소를 저장
+		b.id = 2;		// 클래스 A를 이용해서 만든 인스턴스 공간에 있는 id 값을 2로 변경
+		System.out.println("runReference : " + a.id);	// 그러기에 변수 a도 id 를 가리키고 있기 때문에 값이 1이 아닌 2로 출력
+	}
+	
+	public static void main(String[] args) {
+		runValue();
+		runReference();
+	}
 }
