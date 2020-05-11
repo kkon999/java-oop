1864c56f8c2b62a716b108cad53a5f8aa76d4a35
kkon999 <gon910910@gmail.com>
2020-05-11 23:58:05 +0900

    Java-생성자(Constructor)

---
+++ src/objectprogramming04/Constructor02.java
@@ -0,0 +1,14 @@
+package objectprogramming04;
+
+public class Constructor02 {
+
+
+	
+	public Constructor02() {
+		System.out.println("생성자 호출 성공");
+	}
+
+	public static void main(String[] args) {
+		Constructor02 ce = new Constructor02();
+	}
+}
