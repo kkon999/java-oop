37b84434341e127045d4b43a7310ecd86031090b
kkon999 <gon910910@gmail.com>
2020-05-12 00:19:19 +0900

    Java-Constructor

--- src/objectprogramming04/Constructor02.java
+++ src/objectprogramming04/Constructor02.java
@@ -1,14 +1,13 @@
 package objectprogramming04;
 
 public class Constructor02 {
-
-
 	
 	public Constructor02() {
 		System.out.println("생성자 호출 성공");
 	}
 
 	public static void main(String[] args) {
-		Constructor02 ce = new Constructor02();
+		
+		Constructor02 ce = new Constructor02(); // 생성자 Constructor02()를 호출하여 ce 인스턴스 생성
 	}
 }
