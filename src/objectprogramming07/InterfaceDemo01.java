b0f56e9f6bcf42cdbd09fd0da98bc9659dee2459
kkon999 <gon910910@gmail.com>
2020-05-19 23:51:18 +0900

    Java-Interface01

---
+++ src/objectprogramming07/InterfaceDemo01.java
@@ -0,0 +1,49 @@
+package objectprogramming07;
+
+
+interface Animal {
+    public void cry();
+    public void sleep();
+}
+ 
+class Dog implements Animal{
+ 
+    @Override
+    public void cry() {
+        System.out.println("왈!! 왈왈!! 왈왈~!!! 왈월워뤙왈!!! 멍멍!");
+    }
+    @Override
+    public void sleep() {
+        System.out.println("자신의 집에서 잠을 잡니다.");
+    }
+     
+}
+ 
+class Cat implements Animal{
+ 
+    @Override
+    public void cry() {
+        System.out.println("야옹~~ 옹옹오오오오오옹~~~~~~ 야오우오우오우옹~~~~");
+    }
+    @Override
+    public void sleep() {
+        System.out.println("집사 얼굴 위에서 잠을 잡니다.");
+    }
+     
+}
+
+public class InterfaceDemo01 {
+	
+	public static void main(String[] args) {
+		Animal dog=new Dog();
+        dog.cry();
+        dog.sleep();
+         
+        System.out.println();
+         
+        Animal cat=new Cat();
+        cat.cry();
+        cat.sleep();
+	}
+
+}
