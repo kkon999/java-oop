0b789fdb921ea44fb182d47eaa07fe1b931fbd58
kkon999 <gon910910@gmail.com>
2020-05-28 23:51:21 +0900

    Java-Interface02

---
+++ src/objectprogramming07/InterfaceDemo02.java
@@ -0,0 +1,28 @@
+package objectprogramming07;
+
+
+
+class Car {
+    
+	public int wheel; 
+    public void drive() {
+        System.out.println("매개 변수가 없어서 출발하지 못합니다");
+    }
+}
+ 
+class RedCar extends Car {
+    
+    public void drive(int a) {
+        System.out.println("출발합니다");
+    }
+}
+
+public class InterfaceDemo02 {    
+            
+    public static void main(String []args) {
+    	RedCar cr = new RedCar();
+        
+        cr.drive();
+    }
+}
+
