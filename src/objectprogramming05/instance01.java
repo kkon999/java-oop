1ca40c21aa2c706017e0d840998224141f634a3b
kkon999 <gon910910@gmail.com>
2020-06-08 23:52:17 +0900

    Java-Instance

---
+++ src/objectprogramming05/instance01.java
@@ -0,0 +1,29 @@
+package objectprogramming05;
+
+
+public class instance01 {
+static int number;
+int speed;
+String color;
+ 
+public void speedUp() {
+      System.out.println("속도를 올린다!!");
+}
+public void speedDown() {
+      System.out.println("속도를 내린다!!");
+}
+ 
+public static void main(String[] args) {
+ 
+	
+ 
+	instance01 myCar = new instance01();
+	instance01 yourCar = new instance01();
+	myCar.speed = 10;
+ 
+	System.out.println(myCar.speed);
+	System.out.println(yourCar.speed);
+  
+}
+
+}
