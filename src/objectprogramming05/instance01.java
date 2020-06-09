cc18cd9d76e5ea46ddeefbb8e341ce5626b4e902
kkon999 <gon910910@gmail.com>
2020-06-09 23:54:22 +0900

    오타수정

--- src/objectprogramming05/instance01.java
+++ src/objectprogramming05/instance01.java
@@ -2,28 +2,30 @@ package objectprogramming05;
 
 
 public class instance01 {
-static int number;
-int speed;
-String color;
+	static int number;
+	int speed;
+	String color;
  
-public void speedUp() {
-      System.out.println("속도를 올린다!!");
-}
-public void speedDown() {
-      System.out.println("속도를 내린다!!");
-}
+	public void speedUp() {
+		
+	      System.out.println("속도를 올린다!!");
+	      
+	}
+	public void speedDown() {
+		
+	      System.out.println("속도를 내린다!!");
+	      
+	}
  
-public static void main(String[] args) {
- 
-	
- 
-	instance01 myCar = new instance01();
-	instance01 yourCar = new instance01();
-	myCar.speed = 10;
- 
-	System.out.println(myCar.speed);
-	System.out.println(yourCar.speed);
-  
-}
+	public static void main(String[] args) {
+	  
+		instance01 myCar = new instance01();
+		instance01 yourCar = new instance01();
+		
+		myCar.speed = 10;W
+		System.out.println(myCar.speed);
+		System.out.println(yourCar.speed);
+	  
+	}
 
 }
